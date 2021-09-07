0f225944fd8a5ae27e9ef7b8ee049ed4f43d88ec
Pebesti <82022148+Pebesti@users.noreply.github.com>
2021-09-07 14:51:01 +0200

    1st node modules

---
+++ greet_xola.js
@@ -0,0 +1,8 @@
+module.exports = function(name){
+    return 'Hello, ' + name;
+}
+
+//import the greet module that is in the current folder
+const greet = require('./greet_xola');
+
+console.log(greet('Xola'))
